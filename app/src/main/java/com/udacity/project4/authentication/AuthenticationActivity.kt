958b73312ae60b0a7a90799030a87d78172d7834
Mustafa Raafat <mustafa.raft@hotmail.com>
2022-12-28 12:38:42 +0200

    init firebase authentication

--- app/src/main/java/com/udacity/project4/authentication/AuthenticationActivity.kt
+++ app/src/main/java/com/udacity/project4/authentication/AuthenticationActivity.kt
@@ -1,8 +1,13 @@
 package com.udacity.project4.authentication
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract
+import com.firebase.ui.auth.data.model.FirebaseAuthUIAuthenticationResult
 import com.udacity.project4.R
+import com.udacity.project4.locationreminders.RemindersActivity
 
 /**
  * This class should be the starting point of the app, It asks the users to sign in / register, and redirects the
@@ -13,12 +18,43 @@ class AuthenticationActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_authentication)
-//         TODO: Implement the create account and sign in using FirebaseUI, use sign in using email and sign in using Google
 
-//          TODO: If the user was authenticated, send him to RemindersActivity
+//        Implement the create account and sign in using FirebaseUI, use sign in using email and sign in using Google
+
+        startSignInWithFirebase()
 
 //          TODO: a bonus is to customize the sign in flow to look nice using :
         //https://github.com/firebase/FirebaseUI-Android/blob/master/auth/README.md#custom-layout
+
+    }
+
+    private fun startSignInWithFirebase() {
+        val providersList = arrayListOf(
+            AuthUI.IdpConfig.EmailBuilder().build(),
+            AuthUI.IdpConfig.GoogleBuilder().build()
+        )
+        val signInLauncher =
+            registerForActivityResult(FirebaseAuthUIActivityResultContract()) { res ->
+                this.firebaseOnSignResult(res)
+            }
+        val signInIntent =
+            AuthUI.getInstance()
+                .createSignInIntentBuilder()
+                .setAvailableProviders(providersList)
+                .setIsSmartLockEnabled(false)
+                .build()
+        signInLauncher.launch(signInIntent)
+    }
+
+    //        If the user was authenticated, send him to RemindersActivity
+    private fun firebaseOnSignResult(res: FirebaseAuthUIAuthenticationResult) {
+        val respo = res.idpResponse
+        if (res.resultCode == RESULT_OK) {
+            var intent = Intent(this, RemindersActivity::class.java)
+            finish()
+            startActivity(intent)
+        } else {
+        }
 
     }
 }
