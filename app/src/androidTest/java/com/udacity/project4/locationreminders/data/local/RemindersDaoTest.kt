6a92246ff3a22ac41c37290d0ebf2b6c05218f59
Mustafa Raafat <mustafa.raft@hotmail.com>
2023-01-12 15:46:36 +0200

    add room test

--- app/src/androidTest/java/com/udacity/project4/locationreminders/data/local/RemindersDaoTest.kt
+++ app/src/androidTest/java/com/udacity/project4/locationreminders/data/local/RemindersDaoTest.kt
@@ -5,6 +5,7 @@ import androidx.room.Room
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.filters.SmallTest;
+import com.udacity.project4.locationreminders.data.dto.ReminderDTO
 //import com.udacity.project4.locationreminders.data.dto.ReminderDTO
 
 import org.junit.Before;
@@ -12,6 +13,7 @@ import org.junit.Rule;
 import org.junit.runner.RunWith;
 
 import kotlinx.coroutines.ExperimentalCoroutinesApi;
+import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.test.runBlockingTest
 import org.hamcrest.CoreMatchers.`is`
 import org.hamcrest.CoreMatchers.notNullValue
@@ -25,6 +27,33 @@ import org.junit.Test
 @SmallTest
 class RemindersDaoTest {
 
-//    TODO: Add testing implementation to the RemindersDao.kt
+    private lateinit var database: RemindersDatabase
 
+    @get:Rule
+    var instantExecutorRule = InstantTaskExecutorRule()
+
+    @Before
+    fun dd() {
+        database = Room.inMemoryDatabaseBuilder(
+            ApplicationProvider.getApplicationContext(),
+            RemindersDatabase::class.java
+        ).build()
+    }
+
+    @After
+    fun f() {
+        database.close()
+    }
+
+//    Add testing implementation to the RemindersDao.kt
+    @Test
+    fun saveReminder_toDatabase() {
+        runBlocking {
+            val reminder = ReminderDTO("milk", "get milk", "shop", 1.0, 1.0, "id")
+            database.reminderDao()
+                .saveReminder(reminder)
+            val load = database.reminderDao().getReminderById("id")
+            assertThat(load!!.id, `is`(reminder.id))
+        }
+    }
 }
