70d0402d9af172d4a98987dd88b980e382a37468
Mustafa Raafat <mustafa.raft@hotmail.com>
2023-01-12 16:55:19 +0200

    add repository test

--- app/src/androidTest/java/com/udacity/project4/locationreminders/data/local/RemindersLocalRepositoryTest.kt
+++ app/src/androidTest/java/com/udacity/project4/locationreminders/data/local/RemindersLocalRepositoryTest.kt
@@ -5,6 +5,8 @@ import androidx.room.Room
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.MediumTest
+import com.udacity.project4.locationreminders.data.dto.ReminderDTO
+import com.udacity.project4.locationreminders.data.dto.Result
 //import com.udacity.project4.locationreminders.data.dto.ReminderDTO
 //import com.udacity.project4.locationreminders.data.dto.Result
 import kotlinx.coroutines.Dispatchers
@@ -24,7 +26,33 @@ import org.junit.runner.RunWith
 //Medium Test to test the repository
 @MediumTest
 class RemindersLocalRepositoryTest {
+    private lateinit var repo: RemindersLocalRepository
+    private lateinit var dao: RemindersDao
+    private lateinit var database: RemindersDatabase
 
-//    TODO: Add testing implementation to the RemindersLocalRepository.kt
+    @Before
+    fun setup() {
+        database = Room.inMemoryDatabaseBuilder(
+            ApplicationProvider.getApplicationContext(),
+            RemindersDatabase::class.java
+        ).build()
+        dao = database.reminderDao()
+        repo = RemindersLocalRepository(dao, Dispatchers.Main)
+    }
 
+    @After
+    fun close() {
+        database.close()
+    }
+
+//    Add testing implementation to the RemindersLocalRepository.kt
+    @Test
+    fun saveTo_getFrom_localRepo() {
+        var reminder = ReminderDTO("milk", "get Milk", "store", 1.0, 1.0, "id")
+        runBlocking {
+            repo.saveReminder(reminder)
+
+            assertThat((repo.getReminder("id")as Result.Success).data.title,`is`("milk"))
+        }
+    }
 }
