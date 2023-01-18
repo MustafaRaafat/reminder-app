e6ea18662e35799947be0bf8530d2d10f9d74702
Mustafa Raafat <mustafa.raft@hotmail.com>
2023-01-18 16:37:03 +0200

    add test to viewModels

--- app/src/test/java/com/udacity/project4/locationreminders/reminderslist/RemindersListViewModelTest.kt
+++ app/src/test/java/com/udacity/project4/locationreminders/reminderslist/RemindersListViewModelTest.kt
@@ -1,13 +1,41 @@
 package com.udacity.project4.locationreminders.reminderslist
 
+import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.udacity.project4.locationreminders.MainCoroutineRule
+import com.udacity.project4.locationreminders.data.FakeDataSource
+import com.udacity.project4.locationreminders.data.dto.ReminderDTO
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runBlockingTest
+import org.junit.Before
+import org.junit.Rule
 import org.junit.runner.RunWith
+import org.junit.Test
+import kotlin.test.assertEquals
 
 @RunWith(AndroidJUnit4::class)
 @ExperimentalCoroutinesApi
 class RemindersListViewModelTest {
+    private lateinit var viewModel: RemindersListViewModel
+    private var data = FakeDataSource()
 
-    //TODO: provide testing to the RemindersListViewModel and its live data objects
+    @get:Rule
+    var mainCoroutineRule = MainCoroutineRule()
 
+    @Before
+    fun setUp() {
+        viewModel = RemindersListViewModel(ApplicationProvider.getApplicationContext(), data)
+    }
+
+    //    provide testing to the RemindersListViewModel and its live data objects
+    @Test
+    fun loadReminders_withOnlyOne() {
+        mainCoroutineRule.runBlockingTest {
+            data.deleteAllReminders()
+            var r = ReminderDTO("hello", "mustafa", "at home", 1.0, 1.0, "id")
+            data.saveReminder(r)
+            viewModel.loadReminders()
+            assertEquals(viewModel.remindersList.value!!.size, 1)
+        }
+    }
 }
--- app/src/test/java/com/udacity/project4/locationreminders/savereminder/SaveReminderViewModelTest.kt
+++ app/src/test/java/com/udacity/project4/locationreminders/savereminder/SaveReminderViewModelTest.kt
@@ -1,16 +1,44 @@
 package com.udacity.project4.locationreminders.savereminder
 
+import android.app.Application
+import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.udacity.project4.locationreminders.MainCoroutineRule
+import com.udacity.project4.locationreminders.data.FakeDataSource
+import com.udacity.project4.locationreminders.data.ReminderDataSource
+import com.udacity.project4.locationreminders.reminderslist.ReminderDataItem
 
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.test.runBlockingTest
+import org.junit.Assert
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
 import org.junit.runner.RunWith
+import kotlin.test.assertEquals
 
 @ExperimentalCoroutinesApi
 @RunWith(AndroidJUnit4::class)
 class SaveReminderViewModelTest {
+    private lateinit var viewmodel: SaveReminderViewModel
+    private var data = FakeDataSource()
 
+    @get:Rule
+    var mainCoroutineRule = MainCoroutineRule()
 
-    //TODO: provide testing to the SaveReminderView and its live data objects
+    @Before
+    fun setUp() {
+        viewmodel = SaveReminderViewModel(ApplicationProvider.getApplicationContext(), data)
+    }
 
-
+//    provide testing to the SaveReminderView and its live data objects
+    @Test
+    fun isValidetData() {
+        mainCoroutineRule.runBlockingTest {
+            val reminder = ReminderDataItem("title", "des", "loc", 1.0, 2.0, "fd")
+            val k = viewmodel.validateEnteredData(reminder)
+            Assert.assertEquals(true, k)
+        }
+    }
 }
